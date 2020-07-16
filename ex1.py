512e7f8643a0d581a7dac8234cd2a08e17b26b73
Jherfson Castro <jherfson.castro@gmail.com>
2020-07-16 00:20:27 -0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,4 +1,16 @@
 class Point:
     """Represents a point in 2-D space. """
-    def print_point(p):
-        print('(%g, %g)' % (p.x, p.y))    
+    # def print_point(p):
+    #     print('(%g, %g)' % (p.x, p.y))
+
+class Rectangle:
+    """Represents a rectangle.
+    attributes: width, height, corner.
+    """
+if __name__ == "__main__":
+    box = Rectangle()
+    box.width = 100.0
+    box.height = 200.0
+    box.corner = Point()
+    box.corner.x = 0.0
+    box.corner.y = 0.0
